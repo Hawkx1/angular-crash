ab5442154abc7fb4997c1423675d48b16379a56e
Hawkx1 <gregg.friedman@revature.net>
2021-12-14 14:35:32 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { ButtonComponent } from './componets/button/button.component';
+import { TasksComponent } from './componets/tasks/tasks.component';
+import { TaskItemComponent } from './componets/task-item/task-item.component';
+import { AddTaskComponent } from './componets/add-task/add-task.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
